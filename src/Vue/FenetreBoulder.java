7c4c424eb2f928b87da457285e0a8c733cffa742
NunusPower <anais.galisson@gmail.com>
2015-05-26 14:07:50 +0200

    Creation FenetreBoulder

---
+++ src/Vue/FenetreBoulder.java
@@ -0,0 +1,32 @@
+package Vue;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class FenetreBoulder extends JFrame
+{
+
+	private final JPanel PanelJeu = new JPanel();
+	private final JPanel PanelInformation = new JPanel();
+
+	public FenetreBoulder()
+	{
+		super("Boulder Dash");
+		setSize(1000, 1000);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		getContentPane().add(PanelJeu, BorderLayout.CENTER);
+		PanelJeu.setBackground(Color.WHITE);
+		getContentPane().add(PanelInformation, BorderLayout.EAST);
+
+		setVisible(true);
+	}
+
+	public static void main(String[] args)
+	{
+		FenetreBoulder FB = new FenetreBoulder();
+	}
+}
