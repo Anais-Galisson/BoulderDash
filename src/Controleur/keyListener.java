87cc296730aec931cfe451dd3063778fbe568680
NunusPower <anais.galisson@gmail.com>
2015-05-31 17:35:17 +0200

    matrice + clavier

---
+++ src/Controleur/keyListener.java
@@ -0,0 +1,58 @@
+package Controleur;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class keyListener implements KeyListener
+{
+
+	boolean up = false;
+	boolean down = false;
+	boolean left = false;
+	boolean right = false;
+
+	@Override
+	public void keyPressed(KeyEvent e)
+	{
+
+		if ( e.getExtendedKeyCode() == 38 ) {
+			up = true;
+		}
+		if ( e.getExtendedKeyCode() == 40 ) {
+			down = true;
+		}
+		if ( e.getExtendedKeyCode() == 37 ) {
+			left = true;
+		}
+		if ( e.getExtendedKeyCode() == 39 ) {
+			right = true;
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e)
+	{
+		if ( e.getExtendedKeyCode() == 38 ) {
+			up = false;
+		}
+		if ( e.getExtendedKeyCode() == 40 ) {
+			down = false;
+		}
+		if ( e.getExtendedKeyCode() == 37 ) {
+			left = false;
+		}
+		if ( e.getExtendedKeyCode() == 39 ) {
+			right = false;
+		}
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Modele/Matrice.java
@@ -0,0 +1,165 @@
+package Modele;
+
+public class Matrice
+{
+	private final int x, y;
+	private final char[][] matrice;
+
+	public Matrice(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+		matrice = new char[x][y];
+
+		for ( int i = 0; i < x; i++ ) {
+			for ( int j = 0; j < y; j++ ) {
+				matrice[i][j] = 'x';
+			}
+		}
+	}
+
+	public void afficherMatrice()
+	{
+		System.out.println();
+		for ( int i = 0; i < x; i++ ) {
+			for ( int j = 0; j < y; j++ ) {
+				System.out.print(" " + matrice[i][j]);
+			}
+			System.out.println(" ");
+		}
+	}
+
+	public void placer(int x_placement, int y_placement, char P)
+	{
+		x_placement = x_placement - 1;
+		y_placement = y_placement - 1;
+
+		if ( x_placement < 0 || y_placement < 0 || y_placement > y || x_placement > x ) {
+			System.out.println("ERREUR");
+		} else if ( matrice[x_placement][y_placement] == 'x' ) {
+			matrice[x_placement][y_placement] = P;
+		} else {
+			System.out.println("Bug, pas de 'x' !");
+		}
+
+		//ajouter d'autre conditions..
+
+	}
+
+	public char getCase(int x, int y)
+	{
+		return matrice[x][y];
+	}
+
+	public void deplacerBas()
+	{
+		char tmp;
+		for ( int i = 0; i < x; i++ ) {
+			for ( int j = 0; j < y; j++ ) {
+
+				if ( matrice[i][j] == 'J' ) { // Egale au type joueur, a changer /!\
+					if ( i + 1 < x && matrice[i + 1][j] == 'x' ) { // Si c'est du vide ..
+						tmp = matrice[i][j];
+						matrice[i][j] = 'x';
+						i = i + 1;
+						matrice[i][j] = tmp;
+					} else if ( i + 1 < x && matrice[i + 1][j] == 'D' ) { // Si c'est un diamant...
+						tmp = matrice[i][j];
+						matrice[i][j] = 'x';
+						i = i + 1;
+						matrice[i][j] = 'G';
+						System.out.println("Vous avez trouvé le diamant pas le bas");
+					}
+				}
+			}
+		}
+	}
+
+	public void deplacerHaut()
+	{
+		char tmp;
+		for ( int i = 0; i < x; i++ ) {
+			for ( int j = 0; j < y; j++ ) {
+
+				if ( matrice[i][j] == 'J' ) { // Egale au type joueur, a changer /!\
+					if ( i > 0 && matrice[i - 1][j] == 'x' ) { //vérifier que l'espace est vide
+						tmp = matrice[i][j];
+						matrice[i][j] = 'x';
+						i = i - 1;
+						matrice[i][j] = tmp;
+					} else if ( i > 0 && matrice[i - 1][j] == 'D' ) {
+						tmp = matrice[i][j];
+						matrice[i][j] = 'x';
+						i = i - 1;
+						matrice[i][j] = 'G';
+						System.out.println("Vous avez trouvé le diamant par le haut!");
+					}
+				}
+			}
+		}
+	}
+
+	public void deplacerGauche()
+	{
+		char tmp;
+		for ( int i = 0; i < x; i++ ) {
+			for ( int j = 0; j < y; j++ ) {
+
+				if ( matrice[i][j] == 'J' ) { // Egale au type joueur, a changer /!\
+					if ( j > 0 && matrice[i][j - 1] == 'x' ) { // Vérifier que l'espace est vide
+						tmp = matrice[i][j];
+						matrice[i][j] = 'x';
+						j = j - 1;
+						matrice[i][j] = tmp;
+					} else if ( j > 0 && matrice[i][j - 1] == 'D' ) {
+						tmp = matrice[i][j];
+						matrice[i][j] = 'x';
+						j = j - 1;
+						matrice[i][j] = 'G';
+						System.out.println("Vous avez trouvé le diamant par la gauche!");
+					}
+				}
+			}
+		}
+	}
+
+	public void deplacerDroite()
+	{
+		char tmp;
+		for ( int i = 0; i < x; i++ ) {
+			for ( int j = 0; j < y; j++ ) {
+
+				if ( matrice[i][j] == 'J' ) { // Egale au type joueur, a changer /!\
+					if ( j + 1 < y && matrice[i][j + 1] == 'x' ) { // vérifier que l'espace est vide
+						tmp = matrice[i][j];
+						matrice[i][j] = 'x';
+						j = j + 1;
+						matrice[i][j] = tmp;
+					} else if ( j + 1 < y && matrice[i][j + 1] == 'D' ) {
+						tmp = matrice[i][j];
+						matrice[i][j] = 'x';
+						j = j + 1;
+						matrice[i][j] = 'G';
+						System.out.println("Vous avez gagné le diamant par la droite");
+					}
+				}
+			}
+		}
+	}
+
+	public int getX()
+	{
+		return x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+
+	public char[][] getMatrice()
+	{
+		return matrice;
+	}
+
+}
