bfd520992d187f86d1d02808efb91948c03cb4b7
NunusPower <anais.galisson@gmail.com>
2015-06-11 21:07:34 +0200

    Signed-off-by: NunusPower <anais.galisson@gmail.com>

--- src/Vue/FenetreBoulder.java
+++ src/Vue/FenetreBoulder.java
@@ -26,34 +26,31 @@ public class FenetreBoulder extends JFrame
 	private final Panel PanelInformation = new Panel();
 	BufferedImage bf = null;
 
-
 	public FenetreBoulder()
 	{
 		super("Boulder Dash");
-		setSize(690, 480);
-		//setResizable(false);
+		setSize(680, 480);
+		setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		//setLocationRelativeTo(null);
 		PanelInformation.setBackground(Color.WHITE);
-		PanelInformation.setPreferredSize(new Dimension(200, 500));
-		
+		PanelInformation.setPreferredSize(new Dimension(200, 480));
+
 		//	PanelJeu.setSize(60, 60);
 		PanelJeu.setBackground(Color.blue);
-		PanelJeu.setPreferredSize(new Dimension(800, 300));
+		PanelJeu.setPreferredSize(new Dimension(480, 480));
 		this.getContentPane().add(PanelInformation, BorderLayout.EAST);
 		this.getContentPane().add(PanelJeu, BorderLayout.WEST);
 		this.addKeyListener(keyListen);
 		this.setVisible(true);
-		
+
 		//this.setContentPane(PanelJeu);
-		
 
 		//matrice.remplirMatrice();
 		//matrice.placer(5, 5, diamond);
 		GestionClavier.Trame();
 
-		this.getContentPane().add(PanelJeu);
 	}
 
 }
